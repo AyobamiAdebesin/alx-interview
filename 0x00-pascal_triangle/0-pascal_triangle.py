2b618c399a53cd658f8fa4ad39a6e394aacc61bc
AyobamiAdebesin <froshofficial@gmail.com>
2022-12-22 22:28:50 +0100

    Trying a fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,7 +9,7 @@ def factorial(n):
     if not isinstance(n, int):
         raise TypeError("n must be an integer")
     if n < 0:
-        return False
+        return 1
     if n == 0:
         return 1
     else:
